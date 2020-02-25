e1e004a2e61acb1c9ed207f3d201e9c46ce04bf7
muzammil22 <muzzammilmuneer@gmail.com>
2020-02-25 11:19:37 +0500

    created IBActions and outlets. Calculated BMI

--- BMI-Calculator-LayoutPractice/Controllers/ViewController.swift
+++ BMI-Calculator-LayoutPractice/Controllers/ViewController.swift
@@ -10,11 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var weightSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func weightSliderChanged(_ sender: UISlider) {
+        let weight = String(format: "%i", Int(sender.value))
+        weightLabel.text = "\(weight)kg"
+    }
+    
+    @IBAction func heightSliderChanged(_ sender: UISlider) {
+        let height = String(format: "%.2f", sender.value)
+        
+        heightLabel.text = "\(height)m"
+    }
+    
+    @IBAction func calculateButtonPressed(_ sender: UIButton) {
+        let height = heightSlider.value
+        let weight = weightSlider.value
 
+        let bmi = weight / pow (height, 2)
+        print(bmi)
+    }
 }
 
